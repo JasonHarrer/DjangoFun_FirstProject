1af642cbf64b2c84a796162db17a87e4eeae414b
Jason Harrer <jazzyeagle@JazzyPuter.localdomain>
2021-08-09 07:06:07 -0500

    Updated with ninja bonus for Multiple Apps assignment

--- first_django_app/urls.py
+++ first_django_app/urls.py
@@ -3,10 +3,5 @@ from . import views
 
 urlpatterns = [
                 path('',                    views.index),
-                path('new',                 views.new),
-                path('create',              views.create),
-                path('<int:number>',        views.show),
-                path('<int:number>/edit',   views.edit),
-                path('<int:number>/delete', views.destroy),
               ]
 
--- first_django_app/views.py
+++ first_django_app/views.py
@@ -4,24 +4,4 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h2>Placeholder</h2><p>to later display a list of all blogs')
-
-
-def new(request):
-    return HttpResponse('<h2>Placeholder</h2><p>to display a new form to create a new blog')
-
-
-def create(request):
-    return redirect('/')
-
-
-def show(request, number):
-    return HttpResponse(f'<h2>Placeholder</h2><p>to edit blog {number}')
-
-
-def edit(request, number):
-    return HttpResponse(f'<h2>Placeholder</h2><p>to edit blog {number}')
-
-
-def destroy(request, number):
-    return redirect('/')
+    return redirect('/blogs')
